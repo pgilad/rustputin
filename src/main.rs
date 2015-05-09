5d637cd2a40a9418c86890ce28bce4c6e4f9ecee
Gilad Peleg <giladp007@gmail.com>
2015-05-09 08:50:46 +0300

    Update code

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,53 @@
 extern crate rustc_serialize;
 extern crate regex;
 extern crate time;
+extern crate hyper;
 
-use nickel::*;
+use nickel::{
+    Request,
+    Response,
+    StaticFilesHandler,
+    MiddlewareResult,
+    HttpRouter,
+    Nickel
+};
+
 use std::collections::HashMap;
+use hyper::uri::RequestUri;
 
 fn main() {
     let mut server = Nickel::new();
     let mut router = Nickel::router();
 
     server.utilize(middleware! { |request|
-        println!("logging request: {:?}", request.origin.uri);
+        let path = match request.origin.uri {
+            RequestUri::AbsolutePath(ref val) => val.to_string(),
+            _ => "unknown uri".to_string()
+        };
+        println!("{} {}", request.origin.method, path);
     });
 
     fn handler<'a>(_: &mut Request, res: Response<'a>) -> MiddlewareResult<'a> {
-        let mut data = HashMap::<&str, String>::new();
         let moscow_offset = time::Duration::hours(3);
-        let cur_utc_time = time::now().to_utc().rfc822().to_string();
-        let rus_time = time::now() + moscow_offset;
+        let cur_time = time::now();
+        let cur_utc_time = cur_time.to_utc().rfc822().to_string();
+        let rus_time = cur_time + moscow_offset;
         let formatted_rus_time = rus_time.rfc822().to_string();
+
+        let mut data = HashMap::<&str, String>::new();
         data.insert("time", cur_utc_time);
         data.insert("russian_time", formatted_rus_time);
+
         res.render("templates/index.tpl", &data)
     }
 
     router.get("/", handler);
+
+    // Mount public directory
     server.utilize(StaticFilesHandler::new("public/"));
     server.utilize(router);
-    server.listen("127.0.0.1:6767");
+
+    let address = "127.0.0.1:6767";
+    println!("Launching server at :{}", address);
+    server.listen(address);
 }
